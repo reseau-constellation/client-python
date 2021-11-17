d14c74f636059c4a8c187485766aab39a2d5f84c
julienmalard <julien.malard@mail.mcgill.ca>
2021-11-17 12:19:59 +0100

    Régler coquille et option d'installation automatique pour Node.js

--- constellationPy/serveur.py
+++ constellationPy/serveur.py
@@ -1,3 +1,4 @@
+import platform
 import subprocess
 from typing import Optional, TypedDict, Union, List, Tuple
 
@@ -19,13 +20,39 @@ def _obt_version(commande: Union[str, List[str]], arg="-v") -> Optional[str]:
     print(f"Erreur serveur Constellation: {résultat}")
 
 
+def _installer_nodejs():
+    système_opératoire = platform.system()
+
+    options_installation = {
+        "Linux": [["sudo apt install nodejs npm"]],
+        "Darwin": [["brew update", "brew doctor", "brew install node"]],
+        "Windows": [["scoop install nodejs"], ["cinst nodejs.install"]]
+    }
+
+    for option in options_installation[système_opératoire]:
+        try:
+            for cmd in option:
+                subprocess.run(cmd.split())
+            if obt_version_npm():
+                return
+        except FileNotFoundError:
+            pass  # Si ça n'a pas fonctionné, on essayera la prochaine !
+
+    # Système opératoire inconnu, ou bien rien n'a fonctionné
+    raise OSError(système_opératoire)
+
+
 def _assurer_constellation_installée():
     version_constellation = obt_version_constellation()
     if not version_constellation:
 
         version_npm = obt_version_npm()
         if not version_npm:
-            raise FileNotFoundError("Vous devez installer Node.js au https://nodejs.org/fr/download/.")
+            try:
+                _installer_nodejs()
+            except Exception:
+                # Si on a pas réussi à l'installer pour vous, vous devrez le faire manuellement.
+                raise FileNotFoundError("Vous devez installer Node.js au https://nodejs.org/fr/download/.")
 
         version_yarn = obt_version_yarn()
         if not version_yarn:
@@ -64,7 +91,7 @@ def lancer_serveur(port=None, autoinstaller=True, exe: TypeExe = "constl") -> Tu
     version = obt_version_constellation(exe)
 
     if not version:
-        raise ChildProcessError("Constellation doit être installé sur votre appareil.")
+        raise ChildProcessError("Constellation doit être installée sur votre appareil.")
     cmd = [*exe, "lancer"]
     if port:
         cmd += ["-p", str(port)]
@@ -84,24 +111,24 @@ type_context = TypedDict("type_context", {"port_serveur": Optional[int]})
 context: type_context = {"port_serveur": None}
 
 
-def changer_context(port: int):
+def changer_contexte(port: int):
     if context["port_serveur"] is not None:
-        raise RuntimeError("On ne peut avoir qu'un serveur en context à la fois.")
+        raise RuntimeError("On ne peut avoir qu'un seule serveur en contexte à la fois.")
 
     context["port_serveur"] = port
 
 
-def effacer_context():
+def effacer_contexte():
     context["port_serveur"] = None
 
 
-def obtenir_context() -> Optional[int]:
+def obtenir_contexte() -> Optional[int]:
     return context["port_serveur"]
 
 
 class Serveur(object):
 
-    def __init__(soimême, port=None, autoinstaller=True, exe: TypeExe = "constl"):
+    def __init__(soimême, port: Optional[int] = None, autoinstaller=True, exe: TypeExe = "constl"):
         soimême.port = port
         soimême.autoinstaller = autoinstaller
         soimême.exe = exe
@@ -110,9 +137,9 @@ class Serveur(object):
 
     def __enter__(soimême):
         soimême.serveur, soimême.port = lancer_serveur(soimême.port, soimême.autoinstaller, soimême.exe)
-        changer_context(soimême.port)
+        changer_contexte(soimême.port)
         return soimême
 
     def __exit__(soimême, *args):
-        effacer_context()
+        effacer_contexte()
         soimême.serveur.terminate()
