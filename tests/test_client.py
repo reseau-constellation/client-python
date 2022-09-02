072e6639c5835dfab12468ac73af5c9f17d0ff9b
julienmalard <julien.malard@mail.mcgill.ca>
2022-09-02 14:36:13 +0200

    Progrès tests client

--- tests/test_client.py
+++ tests/test_client.py
@@ -73,16 +73,21 @@ class TestClient(TestCase):
             id_col = await client.tableaux.ajouterColonneTableau(id_tableau=id_tableau, id_variable=id_var)
             print("id_col", id_col)
 
-            async def f_suivre_données(éléments):
+            résultat = {}
+
+            def f_suivre_données(éléments):
+                print("éléments", éléments)
                 if éléments:
-                    print("éléments", éléments)
-                    soimême.assertEqual(éléments[0][id_col], 123)
+                    résultat["élément"] = éléments
+                    soimême.assertEqual(éléments[0]["données"][id_col], 123)
                     oublier_données()
 
-            # oublier_données = await client.tableaux.suivreDonnées(id_tableau=id_tableau, f=f_suivre_données)
+            oublier_données = await client.tableaux.suivreDonnées(id_tableau=id_tableau, f=f_suivre_données)
             print("oublier données")
             id_élément = await client.tableaux.ajouterÉlément(id_tableau=id_tableau, vals={id_col: 123})
             print("id_élément", id_élément)
+
+            soimême.assertIsNotNone(résultat["élément"])
 
     @unittest.skipIf(not VRAI_SERVEUR, "Test uniquement pour le vrai serveur.")
     async def test_obt_données_tableau(soimême):
@@ -114,10 +119,10 @@ class TestClient(TestCase):
                     erreurs.append(json.loads(erreur))
 
         async with trio.open_nursery() as pouponnière:
-            canal_envoie_erreur, canal_reçoie_erreur = trio.open_memory_channel(0)
+            canal_envoie_erreur, canal_reçoit_erreur = trio.open_memory_channel(0)
 
             pouponnière.start_soon(coroutine_client, pouponnière, canal_envoie_erreur)
-            pouponnière.start_soon(coroutine_erreurs, canal_reçoie_erreur)
+            pouponnière.start_soon(coroutine_erreurs, canal_reçoit_erreur)
 
         print(erreurs)
         soimême.assertEqual(len(erreurs), 1)
