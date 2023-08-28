9c03ad358d8e76d93b817a930ab26eca47e557ca
julienmalard <julien.malard@mail.mcgill.ca>
2023-08-28 14:53:55 +0200

    Pour Ubuntu

--- constellationPy/__init__.py
+++ constellationPy/__init__.py
@@ -1,8 +1,11 @@
-from importlib.metadata import version
+from importlib.metadata import version, PackageNotFoundError
 
 from .client import ouvrir_client, Client
 from .serveur import Serveur, lancer_serveur, mettre_constellation_à_jour, désinstaller_constellation
 from .sync import ClientSync
 from .utils import fais_rien, une_fois
 
-__version__ = version("constellationPy")
+try:
+    __version__ = version("constellationPy")
+except PackageNotFoundError:
+    __version__ = None
