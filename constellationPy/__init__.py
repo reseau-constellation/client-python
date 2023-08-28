7e9f27285aff4739e6c30c55b2e6fe7e7c2c6f86
julienmalard <julien.malard@mail.mcgill.ca>
2023-08-28 14:49:12 +0200

    Mises à jour

--- constellationPy/__init__.py
+++ constellationPy/__init__.py
@@ -1,11 +1,8 @@
-import pkg_resources
+from importlib.metadata import version
 
 from .client import ouvrir_client, Client
 from .serveur import Serveur, lancer_serveur, mettre_constellation_à_jour, désinstaller_constellation
 from .sync import ClientSync
 from .utils import fais_rien, une_fois
 
-try:
-    __version__ = pkg_resources.get_distribution('constellationPy').version
-except pkg_resources.DistributionNotFound:
-    __version__ = None
+__version__ = version("constellationPy")
--- constellationPy/client.py
+++ constellationPy/client.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 import inspect
 import json
-import logging
 from contextlib import asynccontextmanager
 from typing import Optional, List, Any, Callable, Dict, Union, Tuple, Awaitable
 from uuid import uuid4
